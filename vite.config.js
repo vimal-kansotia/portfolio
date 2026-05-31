b2e8592822de7a4882b00e40eb807e3ac31bcaa1
Vimal Kansotia <santoshkasotia505@gmail.com>
2026-05-31 19:50:47 +0530

    feat: add Google site verification and analytics tracking, and optimize build configuration for chunk splitting

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,17 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: './',
+  build: {
+    chunkSizeWarningLimit: 1500,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        },
+      },
+    },
+  },
 });
